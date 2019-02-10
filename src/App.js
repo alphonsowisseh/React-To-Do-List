3fc2c62a9b2740377b95ae5b5dab55715fdb484c
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-02-10 14:22:13 -0500

    Revert "Revert "createvtodos object with state""
    This reverts commit f674a9b636a600ef580f8e1c8dd5fb6599945140.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos'
 
 class App extends Component {
   state = {
@@ -26,20 +26,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
