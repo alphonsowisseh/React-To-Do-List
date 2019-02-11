0033e21965fb48e3cb7e663e43ca12cdde242230
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-02-10 23:15:51 -0500

    Update App.js
    Use uuid v4 and create addTodo function

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
+import uuid from 'uuid'
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash.',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with wife.',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss.',
         completed: false
       }
@@ -39,6 +40,15 @@ class App extends Component {
 delTodo = (id) => {
   this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
 }
+
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title,
+    completed: false
+  }
+  this.setState({todos: [...this.state.todos, newTodo]})
+}
   
 
   render() {
@@ -46,7 +56,7 @@ delTodo = (id) => {
       <div className="App">
       <div className='container'>
         <Header />
-        <AddTodo />
+        <AddTodo addTodo={this.addTodo} />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
       </div>
