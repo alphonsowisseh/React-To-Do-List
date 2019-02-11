6d1a5b77365c38c3f5a09ed541776b1b5eadf078
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-02-10 22:37:39 -0500

    Header.js
    Static Header Markup

---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>To Do List</h1>
+    </header>
+  )
+}
+
+const headerStyle = {
+    background: '#333333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+}
