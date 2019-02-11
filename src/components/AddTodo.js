236c38477c3345ebda151a46c066f57307c78bd9
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-02-10 23:15:06 -0500

    Update AddTodo.js
    Change from Functional Component to Class Based Component. Add title state and set it to value typed in input.

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function AddTodo() {
-  return (
-    <form style={{display: 'flex'}}>
-      <input type='text' name='title' placeholder='Add To Do' style={{flex: '10', padding: '5px', border: '1px solid #f4f4f4'}} />
-      <input type='submit' value='Submit' className='btn' style={{flex: '1'}} />
-    </form>
-  )
+export class AddTodo extends Component {
+    state = {
+        title: ''
+    }
+
+    onChange = (e) => this.setState({title: e.target.value});
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title: ''})
+    }
+
+  render() {
+    return (
+        <form style={{display: 'flex'}} onSubmit={this.onSubmit}>
+          <input type='text' name='title' placeholder='Add To Do...' value={this.state.value} onChange={this.onChange} style={{flex: '10', padding: '5px', border: '1px solid #f4f4f4'}} />
+          <input type='submit' value='Submit' className='btn' style={{flex: '1'}} />
+        </form>
+      )
+  }
 }
+
+export default AddTodo
